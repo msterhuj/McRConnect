7acbb3fa2de1c830369f31db1f61e786c761ac0e
msterhuj <gabin.lanore@gmail.com>
2022-05-23 23:22:20 +0200

    added vuejs

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card border-info mb-3" style="max-width: 20rem;">
+          <div class="card-body">
+            <h4 class="card-title">Ip to scan left</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6"></div>
+    </div>
+  </div>
+</template>
